5e00ddb8fe7820d0b21e2ad709d3dc395e275e5a
SAJADnet <sajadsajad.sch@gmail.com>
2026-02-17 22:47:07 +0330

    add __init__.py and __main__.py files

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+TermType - A terminal-based typing speed test
+"""
+
+__version__ = "1.0.0"
+__author__ = "Sajad Chehrazi"
+__license__ = "MIT"
+
+from termtype.app import main
---
+++ __main__.py
@@ -0,0 +1,8 @@
+"""
+Allow running as module: python -m termtype
+"""
+
+from termtype.app import main
+
+if __name__ == "__main__":
+    main()
